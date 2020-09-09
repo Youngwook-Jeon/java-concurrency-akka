8ab1ae4878c9e0a243ee2d922b3fd963ed0ec801
Young wook Jeon <ela87@naver.com>
2020-09-10 02:53:52 +0900

    Add Main class

--- java-racing-simulation/src/main/java/RaceController.java
+++ java-racing-simulation/src/main/java/RaceController.java
@@ -6,6 +6,7 @@ import akka.actor.typed.javadsl.Behaviors;
 import akka.actor.typed.javadsl.Receive;
 
 import java.io.Serializable;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,6 +37,10 @@ public class RaceController extends AbstractBehavior<RaceController.Command> {
         }
     }
 
+    private class GetPositionsCommand implements Command {
+        private static final long serialVersionUID = 1L;
+    }
+
     private RaceController(ActorContext<Command> context) {
         super(context);
     }
@@ -47,6 +52,19 @@ public class RaceController extends AbstractBehavior<RaceController.Command> {
     private Map<ActorRef<Racer.Command>, Integer> currentPositions;
     private long start;
     private int raceLength = 100;
+    private Object TIMER_KEY;
+
+    private void displayRace() {
+        int displayLength = 160;
+        for (int i = 0; i < 50; ++i) System.out.println();
+        System.out.println("Race has been running for " + ((System.currentTimeMillis() - start) / 1000) + " seconds.");
+        System.out.println("    " + new String (new char[displayLength]).replace('\0', '='));
+        int i = 0;
+        for (ActorRef<Racer.Command> racer : currentPositions.keySet()) {
+            System.out.println(i + " : "  + new String (new char[currentPositions.get(racer) * displayLength / 100]).replace('\0', '*'));
+            i++;
+        }
+    }
 
     @Override
     public Receive<Command> createReceive() {
@@ -59,6 +77,16 @@ public class RaceController extends AbstractBehavior<RaceController.Command> {
                                 getContext().spawn(Racer.create(), "racer" + i);
                         currentPositions.put(racer, 0);
                         racer.tell(new Racer.StartCommand(raceLength));
+                    }
+                    return Behaviors.withTimers(timer -> {
+                        timer.startTimerAtFixedRate(TIMER_KEY, new GetPositionsCommand(), Duration.ofSeconds(1));
+                        return this;
+                    });
+                })
+                .onMessage(GetPositionsCommand.class, message -> {
+                    for (ActorRef<Racer.Command> racer : currentPositions.keySet()) {
+                        racer.tell(new Racer.PositionCommand(getContext().getSelf()));
+                        displayRace();
                     }
                     return this;
                 })
